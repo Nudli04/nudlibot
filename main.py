4edaf3a0e1f0a64329ba49599a004fe4ff1397d1
Nudli04 <69215876+Nudli04@users.noreply.github.com>
2021-03-22 14:06:27 +0100

    V1.0.1
    The bot now can check the price and other stuff of a crypto(dogecoin).

--- main.py
+++ main.py
@@ -1,10 +1,36 @@
 import discord
+import requests
+import json
 
 client = discord.Client()
 
 @client.event
 async def on_ready():
   print('Bejelentkeztem {0.user} -ként'.format(client))
+
+def get_price():
+  response = requests.get("https://api.binance.com/api/v3/ticker/24hr?symbol=DOGEUSDT")
+  json_data= response.json()
+  price = json_data['lastPrice']
+  return(price)
+
+def get_prchange():
+  response = requests.get("https://api.binance.com/api/v3/ticker/24hr?symbol=DOGEUSDT")
+  json_data= response.json()
+  prchange = json_data['priceChangePercent']
+  return(prchange)
+
+def get_phigh():
+  response = requests.get("https://api.binance.com/api/v3/ticker/24hr?symbol=DOGEUSDT")
+  json_data= response.json()
+  phigh = json_data['highPrice']
+  return(phigh)
+
+def get_plow():
+  response = requests.get("https://api.binance.com/api/v3/ticker/24hr?symbol=DOGEUSDT")
+  json_data= response.json()
+  plow = json_data['lowPrice']
+  return(plow)
 
 
 @client.event
@@ -16,6 +42,13 @@ async def on_message(message):
     mention = message.mentions[0].id
     response = f"Én is gratulálok neked <@!{mention}>, ügyes vagy"
     await message.channel.send(response)
-
+  
+  if message.content.startswith('$doge'):
+        price = get_price()
+        prchange = get_prchange()
+        phigh = get_phigh()
+        plow = get_plow()
+        await message.channel.send(f"----------DOGECOIN----------\nJelenlegi ár: {price:.7}  USD\nNapi változás: {prchange} %\nMai legmagasabb ár: {phigh:.7} USD\nMai legalacsonyabb ár: {plow:.7} USD\n-----------------------------------")
+  
 client.run(TOKEN)
 
